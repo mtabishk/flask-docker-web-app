d33400eefec455722c47dcc706b8b57a2b4ee609
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-07-17 10:25:10 +0530

    updated

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, session, redirect, jsonify, request
+from passlib.hash import pbkdf2_sha256
+import uuid
+from functools import wraps
+import pymongo
+
+app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+
+# Database
+client = pymongo.MongoClient('localhost', 27017)
+db = client.myusersdb
+
+
+class User:
+
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
+    def signup(self):
+        print(request.form)
+
+        # Create the user object
+        user = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "email": request.form.get('email'),
+            "password": request.form.get('password')
+        }
+
+        # Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+
+        # Check for existing email address
+        if db.users.find_one({"email": user['email']}):
+            return jsonify({"error": "Email address already in use"}), 400
+
+        if db.users.insert_one(user):
+            return self.start_session(user)
+
+        return jsonify({"error": "Signup failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+    def login(self):
+
+        user = db.users.find_one({
+            "email": request.form.get('email')
+        })
+
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+
+        return jsonify({"error": "Invalid login credentials"}), 401
+
+# Decorators
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
+
+
+# Routes
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    return User().signup()
+
+
+@app.route('/user/signout')
+def signout():
+    return User().signout()
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return User().login()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
